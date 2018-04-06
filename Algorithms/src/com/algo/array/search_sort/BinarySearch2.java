f41e471db034f3690ccf9740d9ce4761aada340c
parkash-arjan <parkash.arjan.github@gmail.com>
2018-04-05 22:11:43 -0400

    Search first occurrence if a number in a sorted array.

--- Algorithms/src/com/algo/array/search_sort/BinarySearch2.java
+++ Algorithms/src/com/algo/array/search_sort/BinarySearch2.java
@@ -1,6 +1,6 @@
 package com.algo.array.search_sort;
 
-public class BinarySearch {
+public class BinarySearch2 {
 	public static void main(String[] args) {
 		// int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 9, 10 };
 		// int[] array = new int[] { 1, 3, 4, 5, 6, 7, 8, 9, 10 };
@@ -11,21 +11,21 @@ public class BinarySearch {
 	}
 
 	static public int binarySearch(int[] array, int value) {
-		return binarySearchHelper(array, value, 0, array.length - 1, 0);
+		return binarySearchHelper(array, value, 0, array.length - 1, -1);
 	}
 
-	static private int binarySearchHelper(int[] array, int value, int start, int end, int placeHolder) {
+	static private int binarySearchHelper(int[] array, int value, int start, int end, int indexFound) {
 		if (end < start) {
-			// return -1;
-			return placeHolder;
+			return indexFound;
 		}
 		int mid = (start + end) / 2;
 		if (array[mid] == value) {
-			return mid;
+			indexFound = mid;
+			return binarySearchHelper(array, value, start, mid - 1, indexFound);
 		} else if (array[mid] > value) {
-			return binarySearchHelper(array, value, start, mid - 1, mid);
+			return binarySearchHelper(array, value, start, mid - 1, indexFound);
 		} else {
-			return binarySearchHelper(array, value, mid + 1, end, mid + 1);
+			return binarySearchHelper(array, value, mid + 1, end, indexFound);
 		}
 	}
 }
